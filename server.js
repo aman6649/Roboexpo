08e764731716c4c280b98ea9ef28d41741fad344
aman6649 <aman.ag6649@gmail.com>
2022-12-10 19:01:30 +0530

    df

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,9 +7,9 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.static("client/build"));
+app.use(express.static("./client/build"));
 
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 app.listen(PORT, () => {
