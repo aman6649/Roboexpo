1cf26ef5586547f90c7c51fd3324585f892dfba7
aman6649 <aman.ag6649@gmail.com>
2022-12-10 18:54:15 +0530

    df

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+// require("dotenv").config();
 const express = require("express");
-require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
