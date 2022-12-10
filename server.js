2b359dce3f36b18985fa3c043b38382c9a4cc419
aman6649 <aman.ag6649@gmail.com>
2022-12-10 19:35:42 +0530

    df

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// require("dotenv").config();
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -7,56 +7,58 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// app.use(express.static("./client/build"));
 
-app.get("/", function (req, res) {
-  res.send("GEllls;dv");
-  // res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-app.listen(PORT, () => {
-  console.log("Server is up and running on the port " + PORT);
-});
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 
-// const particiSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [1, "Enter your name"],
-//   },
-//   email: {
-//     type: String,
-//     required: [1, "Enter your email"],
-//   },
-//   phone: {
-//     type: String,
-//     required: [1, "Enter your phone no."],
-//   },
-//   usn: {
-//     type: String,
-//     required: [1, "Enter your usn"],
-//   },
-// });
+const particiSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [1, "Enter your name"],
+  },
+  email: {
+    type: String,
+    required: [1, "Enter your email"],
+  },
+  phone: {
+    type: String,
+    required: [1, "Enter your phone no."],
+  },
+  usn: {
+    type: String,
+    required: [1, "Enter your usn"],
+  },
+});
 
-// const user = mongoose.model("user", particiSchema);
+const user = mongoose.model("user", particiSchema);
 
-// app.post("/registerteam", function (req, res) {
-//   if (req.body.name && req.body.email && req.body.usn && req.body.phone) {
-//     const gp = new user({
-//       name: req.body.name,
-//       email: req.body.email,
-//       usn: req.body.usn,
-//       phone: req.body.phone,
-//     });
-//     gp.save((err) => {
-//       if (err) {
-//         console.log(err);
-//       } else res.sendStatus(200);
-//     });
-//   } else {
-//     res.status(401).json({ message: "Please fill all details" });
-//   }
-// });
+app.post("/registerteam", function (req, res) {
+  if (req.body.name && req.body.email && req.body.usn && req.body.phone) {
+    const gp = new user({
+      name: req.body.name,
+      email: req.body.email,
+      usn: req.body.usn,
+      phone: req.body.phone,
+    });
+    gp.save((err) => {
+      if (err) {
+        console.log(err);
+      } else res.sendStatus(200);
+    });
+  } else {
+    res.status(401).json({ message: "Please fill all details" });
+  }
+});
+app.use(express.static("./client/build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "./client/build/index.html"));
+});
+app.listen(PORT, () => {
+  console.log("Server is up and running on the port " + PORT);
+});
